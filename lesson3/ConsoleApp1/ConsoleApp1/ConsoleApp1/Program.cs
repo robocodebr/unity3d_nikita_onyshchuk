f32a99eb21ee4053347a321f78b61ce16ed045e1
robocodebr <robocodebr@gmail.com>
2020-09-19 16:02:09 +0300

    lesson 3

---
+++ lesson3/ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters;
+using System.Collections.Generic;
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            List<int> list = new List<int>();
+            for(int i = 0; i <= arr.Length; i++)
+                {
+                    list.Add(i);
+                }
+        }
+        //int j = 0;
+            ///do
+            //{
+
+             //   Console.Write(j + " ");
+            ///    j += 5;
+            //} ///while (j <= 20);
+
+
+
+
+            /*  string str = "Nikita Onyshchuk 19/09/2020";
+            string str2 = "28/10/2005";
+            int i = 1;
+            while (i <= 17)
+            {
+                Console.Write(i+" ");
+                i += 4;
+            }
+            */
+
+
+
+
+
+
+
+
+
+                //  for(int i = 0; i <str.Length; i+=4)
+                // {
+                ///      Console.WriteLine(str[i]);
+
+                //  }
+                ///for(int i = 0; i < str2.Length; i+=2)
+                ///   {
+                /// Console.WriteLine(str2[i]);
+                //}
+               // Console.WriteLine("Hello World!");
+           // Console.ReadLine();
+        }
+    }
