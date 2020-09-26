9c5b6914a292b6b0513ae7beebef747239683531
robocodebr <robocodebr@gmail.com>
2020-09-26 15:52:27 +0300

    lesson 4

---
+++ lesson 4/ConsoleA/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> l = new List<string>() {"Химия","Биология","Алгебра","Геометрия"};
+            foreach(string s in l)
+            {
+                Console.Write(s);
+               
+            }
+            Console.WriteLine(l.Count);
+            
+            Console.WriteLine(l[0]);
+            Console.WriteLine(l[1]);
+            Console.WriteLine(l[2]);
+            Console.WriteLine(l[3]);
+
+            l.Clear();
+
+
+            Console.WriteLine("Hello World!");
+            Console.Clear();
+            
+            Console.ReadKey();
+        }
+    }
+}
