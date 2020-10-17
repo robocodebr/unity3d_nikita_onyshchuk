0bb10f2fb5a218ad9c5aa0b0606cd03893bf0b3f
robocodebr <robocodebr@gmail.com>
2020-10-17 16:10:16 +0300

    lesson 7

--- lesson 4/Snake/Snake/Assets/Scripts/AppleLife.cs
+++ lesson 4/Snake/Snake/Assets/Scripts/AppleLife.cs
@@ -7,9 +7,21 @@ public class AppleLife : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        float x = Random.Range(-8, 8);
+        float y = Random.Range(-5, 5);
 
+        // createApple();  
+        AppleObj = Instantiate(ApplePref) as GameObject;
+        transform.position = new Vector3(x, y, 0);
+    }
+   
+    public GameObject ApplePref;
+    GameObject AppleObj;
+
+    void createApple()
+    {
+       
+    }
     // Update is called once per frame
     void Update()
     {
--- lesson 4/Snake/Snake/Assets/Scripts/MainGame.cs
+++ lesson 4/Snake/Snake/Assets/Scripts/MainGame.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
@@ -8,6 +9,12 @@ public class MainGame : MonoBehaviour
     public GameObject SnakeHead;
     GameObject SnakeObj;
     int gameMode = 0;
+    int score = 0;
+    public GameObject ApplePref;
+    int maxTime = 150;
+    int time = 0;
+    int time2 = 0;
+    List<GameObject> Apples = new List<GameObject>();
     void createSnake()
     {
         SnakeObj = Instantiate(SnakeHead) as GameObject;
@@ -22,9 +29,58 @@ public class MainGame : MonoBehaviour
     }
 
     // Update is called once per frame
+
+    float xx = 0, yy = 0;
     void Update()
     {
+        time++;
+        time2++;
+        if (time > +maxTime) 
+        {
+            GameObject apple =Instantiate(ApplePref) as  GameObject        ;
+            Apples.Add(apple); 
+            time = 0;
 
+        }
+        if(time2 >= maxTime * 5)
+        {
+            GameObject apple = Apples[0];
+            Destroy(apple);
+            Apples.Remove(apple);
+            time2 = 0;
+
+        }
+        
+        
+        if (SnakeObj != null)
+        {
+            xx = 0;
+            yy = 0; 
+            
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                xx = 1;
+            }
+            if (Input.GetAxis("Vertical") > 0)
+            {
+                yy = 1;
+            }
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                xx = -1;
+            }
+            if (Input.GetAxis("Vertical") < 0)
+            {
+                yy = -1;
+            }
+            if(xx !=0 || yy != 0)
+            {
+                SnakeLife s = SnakeObj.GetComponent<SnakeLife>();
+                if (xx != 0) { s.direction = new Vector2(xx, 0); }
+                if (yy != 0) { s.direction = new Vector2(0, yy); }
+
+            }
+        }
     }
     void OnGUI()
     {
@@ -33,13 +89,36 @@ public class MainGame : MonoBehaviour
         switch (gameMode)
         {
             case 0:
-                GUI.Button(new Rect(new Vector2(posX - 100, posY),
+            Boolean stateBtn1 = GUI.Button(new Rect(new Vector2(posX - 100, posY),
                                    new Vector2(200, 30)
                                    ), "Старт игры");
-
+                Boolean stateBtn2 = GUI.Button(new Rect(
+                    new Vector2(posX - 100, posY + 50),
+                            new Vector2(200, 30)
+                            ), "Выход");
+                if (stateBtn1)
+                {
+                    createSnake();
+                }
+                if (stateBtn2)
+                {
+                    Application.Quit();
+                }
                 break;
-                                   case 1: break;
+                                   case 1:
+                SnakeLife snakel = SnakeHead.GetComponent<SnakeLife>();
+                if (snakel != null) 
+                {
+                    score = snakel.SnakeScore;
+                    GUI.Label(new Rect(
+                        new Vector2(posX, 0),
+                        new Vector2(50, 30)), "Score: "+score);
+                        }
+                
+                
+                break;
 
         }
+    
     }
 }
